3cb3f47abfdcf83083301ea94726a5b3075f0d71
Toby Allen <toby@cookingisfun.ie>
2026-08-19 17:35:58 +0100

    rename

--- src/Facades/LocalStore.php
+++ src/Facades/LocalStore.php
@@ -3,12 +3,12 @@
   namespace Tobya\WorkWithComposer\Facades;
 
   use Illuminate\Support\Facades\Facade;
-  use Tobya\WorkWithComposer\Services\RepositoryStoreService;
+  use Tobya\WorkWithComposer\Services\WorkStoreService;
 
   class LocalStore extends Facade
   {
     protected static function getFacadeAccessor(): string
     {
-      return RepositoryStoreService::class;
+      return WorkStoreService::class;
     }
   }
--- src/Services/WorkStoreService.php
+++ src/Services/WorkStoreService.php
@@ -5,7 +5,7 @@
   use Illuminate\Support\Arr;
   use Illuminate\Support\Collection;
 
-  class RepositoryStoreService  extends JSONFileReader
+  class WorkStoreService  extends JSONFileReader
   {
 
 
